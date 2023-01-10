44130fd62791d09b756eed206d01105ce03768ef
Satria Winarah <swinarah@gmail.com>
2023-01-10 22:16:57 +0700

    bump

--- src/header/Header.js
+++ src/header/Header.js
@@ -17,7 +17,7 @@ const Header = () => (
                     <Col></Col>
                 </Row>
                 <Row className={"column-name"}>
-                    <Col>Satria Winarah Test 2</Col>
+                    <Col>Satria Winarah Test 3</Col>
                 </Row>
                 <Row className={"column-profession"}>
                     <Col>Fullstack Engineer</Col>
